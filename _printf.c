61752d0da6519f7e8437ee074a1a330d9945995f
Noons-ledev <noah.briet@icloud.com>
2024-11-26 12:34:13 +0100

    Handling modulo + valid specifier case, fixing edge cases on print_string function

--- _printf.c
+++ _printf.c
@@ -12,11 +12,51 @@ int print_string(va_list arg)
 int i = 0;
 char *str = va_arg(arg, char *);
 if (str == NULL)
-return (0);
+str = "(null)";
 while (str[i] != '\0')
 {
 _putchar(str[i]);
 i++
 }
 return (i);
+}
+/**
+ * print_char - print a character
+ * @arg: argument
+ */
+int print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+	_putchar(c);
+	return 1;
+}
+
+int _printf(const char *format, ...)
+{
+int print_func get_print[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_double},
+		{'i', print_int},
+		{'\0', NULL}
+};
+int i = 0, j, count = 0;
+va_list args;
+va_start(args, format);
+while (format && format[i])
+{
+    if (format[i] == '%')
+    {
+    j = 0;
+    while (get_print[j].specifier != '\0')
+    {
+    if (format[i + 1] == get_print[j].specifier)
+    {
+    count += get_print[j].f(args);
+    i += 2;
+    break;
+    }
+    j++;
+    }
+    }
 }
