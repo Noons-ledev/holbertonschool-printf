b24c66e6adca24ad8fba42af4a07dfaa2c59af5b
root <root@Alexandre.>
2024-11-26 11:29:34 +0100

    check invalid specifier

--- _printf.c
+++ _printf.c
@@ -22,3 +22,18 @@ int print_func print_function[] = {
 		{'i', print_int},
 		{'\0', NULL}
 };
+va_list arg;
+int i = 0;
+
+	va_start(arg, format);
+
+	while (format && format[i])
+	{
+	if (format[i] == '%' &&
+	(format[i + 1] != '\0' ||
+	(format[i + 1] != 's' && format[i + 1] != 'c' && format[i + 1] != '%')))
+	{
+	return(-1)
+	}
+	i++;
+	}
